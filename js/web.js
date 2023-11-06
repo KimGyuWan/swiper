cf5cc26cbce9445bc562e6314c466ef6bf2de9e4
KimGyuWan <y5g5776@naver.com>
2023-11-06 12:09:55 +0900

    fade처리

--- js/web.js
+++ js/web.js
@@ -1,21 +1,13 @@
 $(document).ready(function () {
-  // setInterval, animate, css, appendTo
-  // 프로토타입
-  // setInterval(function(){}, speed);
-  // $(태그).animate({"스타일": "스타일값"}, speed, function() {});
-  // $(태그).css("스타일", "스타일값");
-  // $(옮길태그).appendTo($(부모가 될 태그));
 
-  const aniw = $(".swiper-slide").height(); //  $(".swiper-slide").css("height"); height값을 가져와라
+  let count = 0;
 
-  setInterval(function () {
-    $(".swiper-wrapper").animate({ "marginTop": -aniw }, 400, function () {
-      $(".swiper-slide").eq(0).appendTo($(".swiper-wrapper"));
-      $(".swiper-wrapper").css("marginTop", "0");
-    });
-  }, 3000);
+  setInterval(fadeani, 3000);
+
+  function fadeani() {
+    count++;
+    count = count % $(".swiper-slide").length;
+    $(".swiper-slide").eq(count).addClass("on").siblings().removeClass("on");
+  }
 })
-
-
-
 
